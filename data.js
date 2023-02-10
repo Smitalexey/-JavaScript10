c424025367b5fc3ca547a2e3a465e27f4565d830
Aleksey <smit_alexey@mail.ru>
2023-02-10 15:06:28 +0500

    десятая домашка по JavaScript

---
+++ data.js
@@ -0,0 +1,107 @@
+const dataApi = `[
+    {
+      "autor": "Анатоль Франс",
+      "language": "ru",
+      "insult": "Чем дальше я живу, тем яснее мне, что прекрасно только то, что нетрудно понять.",
+      "shown": "3295",
+      "createdby": "",
+      "active": "1",      
+      "image": "https://avatars.mds.yandex.net/i?id=a34daea7e75fe989ab1c281c858b3160144056f2-8192006-images-thumbs&n=13"
+    },
+    {
+      "autor": "Альбер Камю",
+      "language": "ru",
+      "insult": "Красота — это вечность, длящаяся мгновение.",
+      "shown": "3113",
+      "createdby": "",
+      "active": "1",      
+      "image": "https://avatars.mds.yandex.net/i?id=7c17abe9c6d63e8aa547b078e1a32abae4504ff7-8261187-images-thumbs&n=13"
+    },
+    {
+      "autor": "Вильгельм Раабе",
+      "language": "ru",
+      "insult": "Красота, истинное счастье и подлинный героизм не нуждаются в громких словах.",
+      "shown": "3154",
+      "createdby": "",
+      "active": "1",     
+      "image": "https://avatars.mds.yandex.net/i?id=9f6e7009809b146255133d7a12c37cc5d46a1585-7188894-images-thumbs&n=13"
+    },
+    {
+      "autor": "Генри Уодсуорт Лонгфелло",
+      "language": "ru",
+      "insult": "В характере, в манерах, в стиле, во всем самое прекрасное — это простота.",
+      "shown": "3229",
+      "createdby": "",
+      "active": "1",      
+      "image": "https://avatars.mds.yandex.net/i?id=7f53314f248633e37df4455b9ccd4c0ff6b07410-5294162-images-thumbs&n=13"
+    },
+    {
+      "autor": "Герман Гессе",
+      "language": "ru",
+      "insult": "Удивительно, как красота и смерть, радость и тлен необходимы друг другу и друг друга обусловливают.",
+      "shown": "3335",
+      "createdby": "",
+      "active": "1",     
+      "image": "https://avatars.mds.yandex.net/i?id=2d6790d30d33dceefb440f445ed57d5587179f8e-2455092-images-thumbs&n=13"
+    },
+    {
+      "autor": "Джакомо Леопарди",
+      "language": "ru",
+      "insult": "Соответствие своему назначению — сущность всякой красоты.",
+      "shown": "3232",
+      "createdby": "",
+      "active": "1",      
+      "image": "https://avatars.mds.yandex.net/i?id=d61b429e35af46d1f596a87f48b7e866e9cc1fe0-3612431-images-thumbs&n=13"
+    },{
+      "autor": "Джебран Халиль Джебран",
+      "language": "ru",
+      "insult": "В сердце того, кто страстно стремится к красоте, она сияет ярче, чем в глазах созерцающего ее.",
+      "shown": "3360",
+      "createdby": "",
+      "active": "1",      
+      "image": "https://avatars.mds.yandex.net/i?id=f60c4ca82f7ecf50aa44d5ef5ebf1fe6e9b0b337-7663084-images-thumbs&n=13"
+    },
+    {
+      "autor": "Жан Жак Руссо",
+      "language": "ru",
+      "insult": "Доброе — это прекрасное в действии.",
+      "shown": "3303",
+      "createdby": "",
+      "active": "1",
+      "image": "https://avatars.mds.yandex.net/i?id=81f3cd0e9665ee4deea3990fbe22d956e0f2892b-6361230-images-thumbs&n=13"
+    },
+    {
+      "autor": "Жан Кокто",
+      "language": "ru",
+      "insult": "Красота действует даже на тех, кто ее не замечает.",
+      "shown": "3294",
+      "createdby": "",
+      "active": "1",
+      "image": "https://rabotatam.ru/uploads/monthly_2017_06/large.593ba808c4419_.jpg.ce226d978c9a517eb5eaba9d6f169c19.jpg"
+    },{
+      "autor": "Кристиан Нестель Боуви",
+      "language": "ru",
+      "insult": "Увидевший прекрасное — соучастник его создания.",
+      "shown": "3322",
+      "createdby": "",
+      "active": "1",
+      "image": "https://avatars.mds.yandex.net/i?id=e337d4a5bc7721c9622a430c644418ac3f2e6433-7762130-images-thumbs&n=13"
+    },{
+      "autor": "Мария фон Эбнер-Эшенбах",
+      "language": "ru",
+      "insult": "То, что восхищает нас в видимой красоте, — это всегда лишь невидимое.",
+      "shown": "3231",
+      "createdby": "",
+      "active": "1",
+      "image": "https://avatars.mds.yandex.net/i?id=2412d4d3dbf1a60568744da43468b646e95545aa-8497449-images-thumbs&n=13"
+    },
+    {
+      "autor": "Мигель де Сервантес Сааведра",
+      "language": "ru",
+      "insult": "Красота обладает силой и даром вносить мир в сердца.",
+      "shown": "3153",
+      "createdby": "",
+      "active": "1",
+      "image": "https://avatars.mds.yandex.net/i?id=bad861974f70a4e2b0fe36dd0cc242628c260de7-7612999-images-thumbs&n=13"
+    }
+  ]`
